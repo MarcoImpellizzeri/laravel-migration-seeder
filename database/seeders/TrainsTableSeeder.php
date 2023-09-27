b145c91db5bc4e311f1cf6a2eeb861ca0630eb14
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-27 16:32:37 +0200

    problema array

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $company = ['Trenitalia', 'Italo', 'Frecciarossa', 'Trenord'];
+            $city = ['Roma', 'Milano', 'Napoli', 'Palermo', 'Bologna', 'Genova'];
+
+            $train = new Train();
+
+            $train->azienda = $faker->randomElement($company);
+            $train->stazione_partenza = $faker->randomElement($city);
+            $train->stazione_arrivo = $faker->randomElement($city);
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->unique()->randomNumber(5);
+            $train->numero_carrozze = $faker->numberBetween(5, 20);
+            $train->in_orario = $faker->boolean;
+            $train->cancellato = $faker->boolean;
+        }
+    }
+}
